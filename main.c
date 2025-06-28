e301f3ebd12fc9cb9df92c1a77095b9ef0a5faa9
shifat <shifat.dka@gmail.com>
2025-06-29 01:03:14 +0600

    Only ADC

--- main.c
+++ main.c
@@ -13,30 +13,36 @@ void timer_config(void);
 void TIM2_IRQHandler(void);
 void USART1_IRQHandler(void);
 void ADC1_2_IRQHandler(void);
-void Exi_config(void);
 
-uint32_t Rdata=0;
-uint32_t Sdata=3;
-uint8_t ind =1;
+uint32_t Rdata = 0;
+uint32_t Sdata = 0;
 uint32_t Rdata2 = 0;
 uint32_t Sdata2 = 0;
 uint16_t Adc_data = 0;
-uint8_t prs = 0;
-uint8_t pas = 0;
+uint8_t present = 1;
+uint8_t past = 0;
 
 int main(void)
 {
 	En_clock();
 	gpio_setup();
 	systick_config();
-	timer_config();
-	Uart1_config();
-	Exi_config();
-		
+	//timer_config();
+//	Uart1_config();
+	//Uart2_config();
+	ADC_config();
 
 	while (1)
 	{
-
+		GPIOA->ODR |= GPIO_ODR_ODR0;
+		//GPIOB->ODR |= GPIO_ODR_ODR10;
+		
+		if (present == 1){
+				GPIOB->ODR |= GPIO_ODR_ODR10;
+		}
+		else if (present ==0){
+			GPIOB->ODR &= ~GPIO_ODR_ODR10;
+		}
 	}
 
 	return 0;
@@ -59,11 +65,11 @@ void gpio_setup(void)
 	GPIOA->CRL &= ~(GPIO_CRL_CNF1 | GPIO_CRL_MODE1);
 	GPIOA->CRL |= GPIO_CRL_MODE1;
 
-	// PA2 user input 1
+	// PA2 UART2: Tx output AF push-pull 1011
 	GPIOA->CRL &= ~(GPIO_CRL_CNF2 | GPIO_CRL_MODE2);
-	GPIOA->CRL |= GPIO_CRL_CNF2_0;
+	GPIOA->CRL |= GPIO_CRL_CNF2_1 | GPIO_CRL_MODE2;
 
-	// PA3 user input 0;
+	// PA3 UART2 Rx: input Floating 0100
 	GPIOA->CRL &= ~(GPIO_CRL_CNF3 | GPIO_CRL_MODE3);
 	GPIOA->CRL |= GPIO_CRL_CNF3_0;
 
@@ -72,14 +78,14 @@ void gpio_setup(void)
 	GPIOA->CRL |= GPIO_CRL_MODE4;
 
 	// PA5 Tx signal for UART2: GPIO output push pull 0011
-	//GPIOA->CRL &= ~(GPIO_CRL_CNF5 | GPIO_CRL_MODE5);
-	//GPIOA->CRL |= GPIO_CRL_MODE5;
+	GPIOA->CRL &= ~(GPIO_CRL_CNF5 | GPIO_CRL_MODE5);
+	GPIOA->CRL |= GPIO_CRL_MODE5;
 
 	// PA6 Rx signal for UART2: GPIO output push pull 0011
-	//GPIOA->CRL &= ~(GPIO_CRL_CNF6 | GPIO_CRL_MODE6);
-	//GPIOA->CRL |= GPIO_CRL_MODE6;
+	GPIOA->CRL &= ~(GPIO_CRL_CNF6 | GPIO_CRL_MODE6);
+	GPIOA->CRL |= GPIO_CRL_MODE6;
 
-	// PA7 Output for USART1 receive use 0011
+	// PA7 Output for USART2 receive use 0011
 	GPIOA->CRL &= ~(GPIO_CRL_CNF7 | GPIO_CRL_MODE7);
 	GPIOA->CRL |= GPIO_CRL_MODE7;
 
@@ -92,17 +98,17 @@ void gpio_setup(void)
 	GPIOA->CRH |= GPIO_CRH_CNF10_0;
 	
 	//PB0 analog input for ADC 0000
-	//GPIOB->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0);
+	GPIOB->CRL &= ~(GPIO_CRL_CNF0 | GPIO_CRL_MODE0);
 	
 	//PB1 Response on ADC watchdog 0011 (light is very high)
-	//GPIOB->CRL &= ~(GPIO_CRL_CNF1 | GPIO_CRL_MODE1);
-	//GPIOB->CRL |= GPIO_CRL_MODE1;
+	GPIOB->CRL &= ~(GPIO_CRL_CNF1 | GPIO_CRL_MODE1);
+	GPIOB->CRL |= GPIO_CRL_MODE1;
 	
 	//PB5 Response on ADC watchdog on arrived from other stm  (2) 0011
-	//GPIOB->CRL &= ~(GPIO_CRL_CNF5 | GPIO_CRL_MODE5);
-	//GPIOB->CRL |= GPIO_CRL_MODE5;
-//
-	//PB10 external interrupt response
+	GPIOB->CRL &= ~(GPIO_CRL_CNF5 | GPIO_CRL_MODE5);
+	GPIOB->CRL |= GPIO_CRL_MODE5;
+	
+	//PB10 Response on ADC watchdog(light is very low) (3) 0011
 	GPIOB->CRH &= ~(GPIO_CRH_CNF10| GPIO_CRH_MODE10);
 	GPIOB->CRH |= GPIO_CRH_MODE10;
 }
@@ -134,7 +140,7 @@ void timer_config(void)
 {
 	TIM2->CNT = 0;
 	TIM2->PSC = 7200 - 1;
-	TIM2->ARR = 30000;
+	TIM2->ARR = 50000;
 	TIM2->DIER |= TIM_DIER_UIE;
 	TIM2->CR1 |= TIM_CR1_DIR;
 	TIM2->CR1 |= TIM_CR1_CEN;
@@ -147,28 +153,27 @@ void TIM2_IRQHandler(void)
 	if (TIM2->SR & TIM_SR_UIF)
 	{
 		TIM2->SR &= ~TIM_SR_UIF;
-		
-		GPIOA->ODR |= GPIO_ODR_ODR0;
-		
-		
-		USART1->DR = Sdata;
 
+		// GPIOA->ODR ^= GPIO_ODR_ODR4;
+		//  Sending signal using USART1
+		GPIOA->ODR |= GPIO_ODR_ODR0;
+		GPIOA->ODR |= GPIO_ODR_ODR5;
+		Sdata ^= 1;
+		Sdata2 ^= 1;
+		USART1->DR = Sdata;
+		USART2->DR = Sdata2;
 		delay(25);
 		GPIOA->ODR &= ~GPIO_ODR_ODR0;
-			
-		Sdata++;
-		if(Sdata==4){
-			Sdata=0;
-		}
-		
+		GPIOA->ODR &= ~GPIO_ODR_ODR5;
 	}
 }
 void Uart1_config(void)
 {
 	USART1->CR1 |= 	USART_CR1_TE 
 								| USART_CR1_RE 
-								|	USART_CR1_RXNEIE 
-								| USART_CR1_UE;
+								| USART_CR1_RXNEIE 
+								| USART_CR1_UE 
+								| USART_CR1_TCIE;
 
 	// Baud rate is 9600;
 	USART1->BRR = 0x1DCC;
@@ -186,54 +191,98 @@ void USART1_IRQHandler(void)
 
 		// Receving data from USART1 rx
 		GPIOA->ODR |= GPIO_ODR_ODR1;
-	
-		if (Rdata == 0 ){
-			GPIOA->ODR &= ~GPIO_ODR_ODR4;
-			GPIOA->ODR &= ~GPIO_ODR_ODR7;
-		}
-		else if( Rdata == 1){
-			GPIOA->ODR |= GPIO_ODR_ODR4;
-			GPIOA->ODR &= ~GPIO_ODR_ODR7;
-		}
-		else if( Rdata == 2){
-			GPIOA->ODR &= ~GPIO_ODR_ODR4;
-			GPIOA->ODR |= GPIO_ODR_ODR7;
-		}
-				else if( Rdata == 3){
-			GPIOA->ODR |= GPIO_ODR_ODR4;
-			GPIOA->ODR |= GPIO_ODR_ODR7;
-		}
-				
-		if(Rdata == 'A'){
+		/*		
+		if (Rdata == 1 | Rdata == 0)
+		{
 			GPIOA->ODR ^= GPIO_ODR_ODR4;
-			GPIOA->ODR ^= GPIO_ODR_ODR7;
+		}*/
+		
+		if(Rdata == 2){
+		GPIOB->ODR |= GPIO_ODR_ODR5;
+		GPIOB->ODR &= ~GPIO_ODR_ODR10;
+		}
+		if(Rdata == 3){
+		GPIOB->ODR |= GPIO_ODR_ODR10;
+		GPIOB->ODR &= ~GPIO_ODR_ODR5;
 		}
 		
 		delay(25);
 		GPIOA->ODR &= ~GPIO_ODR_ODR1;
 	}
-}
-
-void Exi_config(void){
- AFIO->EXTICR[1] = 0;
- 
 	
-EXTI->IMR |= EXTI_IMR_MR2 | EXTI_IMR_MR3;
-EXTI->RTSR |= EXTI_RTSR_TR2 | EXTI_RTSR_TR3;
-
-NVIC_EnableIRQ(EXTI2_IRQn);
-//NVIC_EnableIRQ(EXTI3_IRQn);
-
-}
-
-void EXTI2_IRQHandler(void){
-	if(EXTI->PR & EXTI_PR_PR2){
-			EXTI->PR |= EXTI_PR_PR2;
-	}
+		if (USART1->SR & USART_SR_TC)
+	{ 
+		USART1->SR &= ~USART_SR_TC;
 		GPIOA->ODR |= GPIO_ODR_ODR0;
-		USART1->DR = 'A';
-		delay(25);
-		GPIOA->ODR &= ~GPIO_ODR_ODR0;
-
+		delay(50);
+		GPIOA->ODR &= ~GPIO_ODR_ODR0;		
+		delay(50);
+	}
 }
 
+void Uart2_config(void)
+{
+	USART2->CR1 |= USART_CR1_TE | USART_CR1_RE | USART_CR1_RXNEIE | USART_CR1_UE;
+	// Baud rate is 9600;
+	USART2->BRR = 0x1DCC;
+
+	NVIC_EnableIRQ(USART2_IRQn);
+}
+
+void USART2_IRQHandler(void)
+{
+
+	if (USART2->SR & USART_SR_RXNE)
+	{
+		USART2->SR &= ~USART_SR_RXNE;
+
+		// Receving data from USART1 rx
+		GPIOA->ODR |= GPIO_ODR_ODR6;
+		Rdata2 = USART2->DR;
+		if (Rdata2 == 1 | Rdata2 == 0)
+		{
+			GPIOA->ODR ^= GPIO_ODR_ODR7;
+		}
+		delay(25);
+		GPIOA->ODR &= ~GPIO_ODR_ODR6;
+	}
+}
+
+
+void ADC_config(void){
+ADC1->CR1 |= 	ADC_CR1_AWDEN 
+						| ADC_CR1_AWDIE
+						| ADC_CR1_AWDCH_3;
+
+ADC1->SQR3 = ADC_SQR3_SQ1_3;
+
+ADC1->CR2 |= ADC_CR2_CONT;
+
+ADC1->CR2 |= ADC_CR2_ADON;
+
+ADC1->HTR = 0xB00;
+ADC1->LTR = 0x800;
+
+delay(500);
+ADC1->CR2 |= ADC_CR2_ADON;
+	
+NVIC_EnableIRQ(ADC1_2_IRQn);
+}
+
+void ADC1_2_IRQHandler(void)
+{
+	Adc_data = ADC1->DR;
+
+    if (ADC1->SR & ADC_SR_AWD) {
+			if(Adc_data > ADC1->HTR){
+			 present = 1;  
+			}
+			else if (Adc_data < ADC1->LTR){
+				present = 0;
+			}
+			
+			past = present;
+			ADC1->SR &= ~ADC_SR_AWD;
+    }
+
+	}
